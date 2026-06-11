9e42b5878ee10064e51f0d97c1b22c139031b0e8
anisdjl <adjelili954@gmail.com>
2026-06-11 12:30:43 +0200

    ex01 doesn't work as excpected

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,11 +9,10 @@ int main(void)
 	
 	uintptr_t ptr = Serializer::serialize(test);
 	std::cout << "adresse de ptr apres serialize " << ptr << std::endl;
-	
 
 	Data *de_serialize = Serializer::deserialize(ptr);
 	std::cout << "adresse apres desirialize " << de_serialize << std::endl;
-
+	
 	delete test;
 	return (0);
 }
